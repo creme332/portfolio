e7870a9229ee98f3ee9fd5c9aac2d5178027fd89
creme332 <65414576+creme332@users.noreply.github.com>
2023-11-05 19:19:12 +0400

    initial setup

--- README.md
+++ README.md
@@ -16,5 +16,10 @@ A neobrutalist personal website.
 - [ ] Run lighthouse
 - [ ] Add site to google search console
 
-## Credits
-UI was inspired by plant22.co.
+## To-do
+After installing the project, perform the following actions:
+- [ ] Update font
+- [ ] Run lighthouse report
+
+## References
+- https://stackoverflow.com/a/16532884/17627866
---
+++ pages/_app.tsx
@@ -0,0 +1,31 @@
+import "@mantine/core/styles.css";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
+import { theme } from "../theme";
+import classes from "../styles/global.css";
+
+interface AppProps {
+  Component: () => JSX.Element;
+  pageProps: any;
+}
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <MantineProvider theme={theme}>
+      <Head>
+        <meta charSet="utf-8" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧊</text></svg>"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Welcome to the official website of creme332, an ambitious software developer dedicated to crafting innovative solutions. Explore my portfolio and projects as I delve into the world of technology, aiming to create impactful contributions within the realm of software development."
+        />
+        <title>creme332</title>
+      </Head>
+      <Component className={classes} {...pageProps} />
+    </MantineProvider>
+  );
+}
