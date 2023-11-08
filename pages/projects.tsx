36a4506b60d71ef39389559da490ff97efb883a7
creme332 <65414576+creme332@users.noreply.github.com>
2023-11-08 20:42:32 +0400

    display shapes on projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,8 +1,20 @@
 import styles from "../styles/Projects.module.css";
 import { motion } from "framer-motion";
 import MyCloseButton from "../components/CloseButton";
+import { Image, Flex, Title } from "@mantine/core";
+import Link from "next/link";
 
 export default function projectPage() {
+  const projects = [
+    "project1",
+    "project1",
+    "project1",
+    "project1",
+    "project1",
+    "project1",
+    "project1",
+  ];
+
   return (
     <motion.div
       initial={{ height: "50%", width: "50%", right: 0, top: 0 }}
@@ -10,7 +22,23 @@ export default function projectPage() {
       transition={{ duration: 0.5 }}
       className={styles.project}
     >
-      <MyCloseButton/>
+      <Title>projects</Title>
+      <MyCloseButton />
+      <Flex wrap={"wrap"} justify={"space-between"}>
+        {projects.map((project, id) => (
+          <Link key={`project-${id}-${project}`} href={`/projects/${project}`}>
+            <div className={styles.shapeContainer}>
+              <Image
+                className={styles.shape}
+                w={300}
+                h={240}
+                src={`/shape-${id}.svg`}
+              />
+              <div className={styles.shapeText}>{project}</div>
+            </div>
+          </Link>
+        ))}
+      </Flex>
     </motion.div>
   );
 }
