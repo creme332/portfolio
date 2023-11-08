4f83608b9775b492a3b4e7cb318c9b56f6fa57dd
creme332 <65414576+creme332@users.noreply.github.com>
2023-11-08 16:19:48 +0400

    complete header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,10 +2,23 @@ import { Flex } from "@mantine/core";
 import styles from "../styles/Header.module.css";
 
 export default function myHeader() {
+  const links = [
+    { name: "email", url: "" },
+    { name: "resume", url: "/resume" },
+    { name: "linkedin", url: "/linkedin" },
+    { name: "github", url: "/github" },
+  ];
+  const a = links.map((link) => (
+    <a key={`nav-key-${styles.name}`} className={styles.link} href={link.url}>
+      {link.name}
+    </a>
+  ));
   return (
     <header className={styles.header}>
-      <Flex className={styles.headerLeft}>logo</Flex>
-      <Flex className={styles.headerRight}>additional info</Flex>
+      <Flex className={styles.headerLeft}>
+        <div className={styles.logo}>creme332</div>
+      </Flex>
+      <Flex className={styles.headerRight}>{a}</Flex>
     </header>
   );
 }
