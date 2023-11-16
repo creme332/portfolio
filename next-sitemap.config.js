25ffa36370a4ab9ac296e71b921fa19003126530
creme332 <65414576+creme332@users.noreply.github.com>
2023-11-16 16:18:25 +0400

    setup next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://creme332.vercel.app",
+  generateRobotsTxt: true, // (optional)
+  generateIndexSitemap: false,
+  sitemapSize: 1000,
+};
