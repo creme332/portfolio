1366dfee521738c9969de7d2396cde3f5fed7656
creme332 <65414576+creme332@users.noreply.github.com>
2023-11-17 11:37:21 +0400

    reduce unused css with purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -10,5 +10,26 @@ module.exports = {
         "mantine-breakpoint-xl": "88em",
       },
     },
+    "postcss-flexbugs-fixes": {},
+    "postcss-preset-env": {
+      autoprefixer: {
+        flexbox: "no-2009",
+      },
+      stage: 3,
+      features: {
+        "custom-properties": false,
+      },
+    },
+    "@fullhuman/postcss-purgecss": {
+      content: [
+        "./pages/**/*.{js,jsx,ts,tsx}",
+        "./components/**/*.{js,jsx,ts,tsx}",
+      ],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+      safelist: ["html", "body"],
+    },
   },
 };
+// References
+// 1. https://purgecss.com/guides/next.html
+// 2. https://stackoverflow.com/q/69725289/17627866
