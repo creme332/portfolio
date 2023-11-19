d838b95f0693291175610d19bc83a776549ae70b
creme332 <65414576+creme332@users.noreply.github.com>
2023-11-19 14:23:00 +0400

    add fallback offline page for pwa

---
+++ pages/_offline.tsx
@@ -0,0 +1,28 @@
+import { Container, Title, Text, Button } from "@mantine/core";
+import classes from "../styles/NotFound.module.css";
+import Link from "next/link";
+
+export default function OfflinePage() {
+  return (
+    <div className={classes.container}>
+      <Container className={classes.root}>
+        <Title order={2} className={classes.title}>
+          Something is not right...
+        </Title>
+        <Text c="dimmed" size="lg">
+          The page that you are trying to open was not cached.
+        </Text>
+        <Button
+          component={Link}
+          href="/"
+          variant="outline"
+          size="md"
+          mt="xl"
+          className={"myButton"}
+        >
+          Go back to home page
+        </Button>
+      </Container>
+    </div>
+  );
+}
